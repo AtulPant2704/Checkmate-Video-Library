6d2c6aae6c50eefc124be6310c8e620286100ff9
AtulPant2704 <atulpant1999@gmail.com>
2022-03-26 11:12:03 +0530

    fix: gave alt tag to image

--- src/components/HorizontalVideoCard/HorizontalVideoCard.jsx
+++ src/components/HorizontalVideoCard/HorizontalVideoCard.jsx
@@ -5,7 +5,7 @@ const HorizontalVideoCard = () => {
         <div className="liked-video">
             <div className="video-description">
                 <div className="img-container">
-                    <img src="http://i3.ytimg.com/vi/tSRV1LSpWt8/hqdefault.jpg" alt="" className="img-responsive video-img" />
+                    <img src="http://i3.ytimg.com/vi/tSRV1LSpWt8/hqdefault.jpg" alt="video-card-img" className="img-responsive video-img" />
                 </div>
                 <div className="video-info">
                     <h3 className="video-title">Grand Prix 22 celebrating Vidit success</h3>
