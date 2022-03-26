3459edd9274ec204d5e9752a8ef2ba96124becfa
AtulPant2704 <atulpant1999@gmail.com>
2022-03-26 10:31:50 +0530

    chore: exports in one file

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export { Navbar } from "./Navbar/Navbar";
 export { Footer } from "./Footer/Footer";
 export { Drawer } from "./Drawer/Drawer";
-export { VideoCard } from "./VideoCard/VideoCard"
+export { VideoCard } from "./VideoCard/VideoCard";
+export { HorizontalVideoCard } from "./HorizontalVideoCard/HorizontalVideoCard";
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,7 @@
 export { Login } from "./Authentication/Login";
 export { SignUp } from "./Authentication/SignUp";
 export { Explore } from "./VideoListing/Explore";
+export { LikedVideos } from "./LikedVideos/LikedVideos";
+export { WatchLater } from "./WatchLater/WatchLater";
+export { History } from "./History/History";
+export { Playlists } from "./Playlists/Playlists";
