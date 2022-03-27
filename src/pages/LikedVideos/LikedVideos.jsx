8faab471fd4013e2e576b59990c481074474ecde
AtulPant2704 <atulpant1999@gmail.com>
2022-03-27 22:06:00 +0530

    feat: display likedVideos

--- src/pages/LikedVideos/LikedVideos.jsx
+++ src/pages/LikedVideos/LikedVideos.jsx
@@ -1,9 +1,16 @@
 import "./LikedVideos.css";
+import { useEffect } from "react";
 import { playlistImage } from "../../assets";
-import { HorizontalVideoCard } from "../../components/index";
-import { Drawer } from "../../components";
+import { useLikes, useAuth } from "../../hooks";
+import { Drawer, HorizontalVideoCard } from "../../components";
+import { getLikesHandler } from "../../utils";
 
 const LikedVideos = () => {
+    const { likesState: { likes }, likesDispatch } = useLikes();
+    const { authState: { token } } = useAuth();
+
+    useEffect(() => getLikesHandler(token, likesDispatch), []);
+
     return (
         <main>
             <div className="likedVideo-page">
@@ -12,15 +19,16 @@ const LikedVideos = () => {
                     <div className="likeVideo-intro">
                         <img src={playlistImage} alt="liked-video-image" className="img-responsive" />
                         <h2>Liked Videos</h2>
-                        <h4>3 videos</h4>
+                        <h4>{likes.length} Videos</h4>
                     </div>
                     <div className="likedVideos-container">
-                        <HorizontalVideoCard />
-                        <HorizontalVideoCard />
-                        <HorizontalVideoCard />
-                        <HorizontalVideoCard />
-                        <HorizontalVideoCard />
-                        <HorizontalVideoCard />
+                        {likes.length > 0 ?
+                            likes.map(video => (
+                                <HorizontalVideoCard key={video._id}{...video} />
+                            )) :
+                            <div className="empty-likes">
+                                <h2>No Liked Videos.</h2>
+                            </div>}
                     </div>
                 </section>
             </div>
