e738fd555a17cdc8eee3f89d5830402edd36cd20
AtulPant2704 <atulpant1999@gmail.com>
2022-03-27 13:48:22 +0530

    feta: get videos server call

---
+++ src/utils/getVideos.js
@@ -0,0 +1,18 @@
+import { getVideosService } from "../services";
+
+const getVideos = async (setVideos) => {
+    try {
+        const response = await getVideosService();
+        if (response.status === 200) {
+            setVideos(response.data.videos);
+        }
+        else {
+            throw new Error();
+        }
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+export { getVideos };
