7c20423720b5c56c92d0dcc0fc0a5552aee043d9
AtulPant2704 <atulpant1999@gmail.com>
2022-03-27 13:49:14 +0530

    feat: wrapped App in videos context

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { makeServer } from "./server";
 import { BrowserRouter as Router } from "react-router-dom";
+import { VideosProvider } from "./hooks";
 
 // Call make Server
 makeServer();
@@ -10,7 +11,9 @@ makeServer();
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <VideosProvider>
+        <App />
+      </VideosProvider>
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
