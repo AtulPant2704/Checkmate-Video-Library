a2e5e504581b365b3a5a0aa434e36351c17c6e4d
AtulPant2704 <atulpant1999@gmail.com>
2022-03-27 13:46:25 +0530

    feat: add channel images

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,7 @@
 import chessbase from "./chessbase.webp";
+import chessTalk from "./chessTalk.webp";
+import ericRosen from "./ericRosen.webp";
+import gothamChess from "./gothamChess.webp";
 import playlistImage from "./chess-dark-brown-light-pieces.webp";
 
-export { chessbase, playlistImage };
+export { chessbase, chessTalk, ericRosen, gothamChess, playlistImage };
