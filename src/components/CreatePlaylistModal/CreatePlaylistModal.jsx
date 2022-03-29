b0c8b67eed520c1c26886d2ee24c241dfc71861e
AtulPant2704 <atulpant1999@gmail.com>
2022-03-29 12:21:08 +0530

    feat: check to add or remove video from playlist

--- src/components/CreatePlaylistModal/CreatePlaylistModal.jsx
+++ src/components/CreatePlaylistModal/CreatePlaylistModal.jsx
@@ -1,16 +1,17 @@
 import "./CreatePlaylistModal.css";
 import { useState } from "react";
-import { useAuth, usePlaylists } from "../../hooks";
-import { createNewPlaylistHandler } from "../../utils";
+import { useAuth, usePlaylists, usePlaylistModal } from "../../hooks";
+import { createNewPlaylistHandler, addVideoToPlaylistHandler, deleteVideoFromPlaylistHandler } from "../../utils";
 
-const CreatePlaylistModal = ({ setPlaylistModal }) => {
+const CreatePlaylistModal = () => {
     const [openCreatePlaylist, setOpenCreatePlaylist] = useState(false);
     const [newPlaylist, setNewPlaylist] = useState({
         title: "",
-        description: ""
+        description: "haha"
     })
     const { authState: { token } } = useAuth();
     const { playlistsState: { playlists }, playlistsDispatch } = usePlaylists();
+    const { playlistModalState: { video }, playlistModalDispatch } = usePlaylistModal();
 
     const playlistNameHandler = (event) => {
         setNewPlaylist({ ...newPlaylist, title: event.target.value })
@@ -21,11 +22,26 @@ const CreatePlaylistModal = ({ setPlaylistModal }) => {
         setOpenCreatePlaylist(false);
     }
 
+    const checkVideoInPlaylist = (_id) => {
+        const playlist = playlists.find(item => item._id === _id);
+        const isExist = playlist.videos.find(item => item._id === video._id)
+        return isExist ? true : false;
+    }
+
+    const callVideoAction = (_id) => {
+        if (checkVideoInPlaylist(_id)) {
+            deleteVideoFromPlaylistHandler(_id, video._id, token, playlistsDispatch);
+        }
+        else {
+            addVideoToPlaylistHandler(token, _id, video, playlistsDispatch);
+        }
+    }
+
     return (
         <div className="playlist-modal">
             <div className="playlist-header">
                 <h3>Save to...</h3>
-                <button className="playlist-modal-close" onClick={() => setPlaylistModal(false)}><i className="fa-solid fa-xmark"></i></button>
+                <button className="playlist-modal-close" onClick={() => playlistModalDispatch({ type: "CLOSE_MODAL" })}><i className="fa-solid fa-xmark"></i></button>
             </div>
             <div className="playlist-container">
                 <div className="playlist-one">
@@ -34,8 +50,8 @@ const CreatePlaylistModal = ({ setPlaylistModal }) => {
                 </div>
                 {playlists.map(({ _id, title }) => (
                     <div className="playlist" key={_id}>
-                        <input type="checkbox" id={_id} />
-                        <label htmlFor={_id}>{title}</label>
+                        <input type="checkbox" id={title} checked={checkVideoInPlaylist(_id)} onChange={() => callVideoAction(_id)} />
+                        <label htmlFor={title}>{title}</label>
                     </div>
                 ))}
             </div>
