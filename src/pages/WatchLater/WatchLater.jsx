3da40a5d50f11007fd6ade8006748e3e8a68a990
AtulPant2704 <atulpant1999@gmail.com>
2022-03-30 17:13:37 +0530

    feat: display watch later videos

--- src/pages/WatchLater/WatchLater.jsx
+++ src/pages/WatchLater/WatchLater.jsx
@@ -1,9 +1,16 @@
 import "./WatchLater.css";
+import { useEffect } from "react";
 import { playlistImage } from "../../assets";
-import { HorizontalVideoCard } from "../../components/index";
-import { Drawer } from "../../components";
+import { useWatchLater, useAuth } from "../../hooks";
+import { Drawer, HorizontalVideoCard } from "../../components";
+import { getWatchLaterHandler } from "../../utils";
 
 const WatchLater = () => {
+    const { authState: { token } } = useAuth();
+    const { watchLaterState: { watchLater }, watchLaterDispatch } = useWatchLater();
+
+    useEffect(() => getWatchLaterHandler(token, watchLaterDispatch), [])
+
     return (
         <main>
             <div className="watchLaterVideo-page">
@@ -12,15 +19,16 @@ const WatchLater = () => {
                     <div className="watchLaterVideo-intro">
                         <img src={playlistImage} alt="watch-later-video-image" className="img-responsive" />
                         <h2>Watch Later Videos</h2>
-                        <h4>3 videos</h4>
+                        <h4>{watchLater.length} Videos</h4>
                     </div>
                     <div className="watchLaterVideos-container">
-                        <HorizontalVideoCard />
-                        <HorizontalVideoCard />
-                        <HorizontalVideoCard />
-                        <HorizontalVideoCard />
-                        <HorizontalVideoCard />
-                        <HorizontalVideoCard />
+                        {watchLater.length > 0 ?
+                            watchLater.map(video => (
+                                <HorizontalVideoCard key={video._id} {...video} />
+                            )) :
+                            <div className="empty-likes">
+                                <h2>No videos in Watch Later.</h2>
+                            </div>}
                     </div>
                 </section>
             </div>
