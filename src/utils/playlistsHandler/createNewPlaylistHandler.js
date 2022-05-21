55533c8cc2b2838f6354cc36f3c815b170c5c9bc
AtulPant2704 <atulpant1999@gmail.com>
2022-05-21 12:29:52 +0530

    feat: show only one toast on new playlist

--- src/utils/playlistsHandler/createNewPlaylistHandler.js
+++ src/utils/playlistsHandler/createNewPlaylistHandler.js
@@ -16,7 +16,6 @@ const createNewPlaylistHandler = async (
         type: "CREATE_NEW_PLAYLISTS",
         payload: response.data.playlists,
       });
-      toast.success("Playlist created");
       const playlists = await getPlaylistsHandler(token, playlistsDispatch);
       const newPlaylist = playlists.find(
         (item) => item.title === playlist.title
@@ -25,8 +24,10 @@ const createNewPlaylistHandler = async (
         token,
         newPlaylist._id,
         video,
-        playlistsDispatch
+        playlistsDispatch,
+        "newPlaylist"
       );
+      toast.success("Playlist created and Video added");
     } else {
       throw new Error();
     }
